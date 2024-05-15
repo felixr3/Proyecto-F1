8c82b86e9953f886eff0894dccb89c5942c71ff4
Juan.sartor <Juansartor92@gmail.com>
2024-05-16 01:29:54 +0200

    - agrego el footer - agregue ajustes responsive para los distintos cortes - cree nueva hoja de estilos

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const header = document.querySelector('header')
 
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', this.window.scrollY > 80)
 })
+
+
+
+
+
+// Desplazamiento suave hacia arriba
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
